105401c3ca594b738782d21719ff71b6edf677cf
SkywardSyntax <shubdeshpa@gmail.com>
2025-12-02 16:46:37 -0600

    fixed joystick forward

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -103,7 +103,7 @@ public class RobotContainer {
                     double xMagnitude = Math.pow(magnitude,2) * Math.cos(angle); // squares magnitude, then multiplies by cos(angle) to get x mag
                     double yMagnitude = Math.pow(magnitude,2) * Math.sin(angle); // squares magnitude, then multiplies by sin(angle) to get y mag
                     
-                    return drive.withVelocityX(-(yMagnitude) * MaxSpeed) // Drive forward with squared Y (maintaining sign)
+                    return drive.withVelocityX((yMagnitude) * MaxSpeed) // Drive forward with squared Y (maintaining sign)
                     .withVelocityY(-(xMagnitude) * MaxSpeed) // Drive left with squared X (maintaining sign)
                     .withRotationalRate(-Math.copySign(joystick.getRightX() * joystick.getRightX(), joystick.getRightX()) * MaxAngularRate); // Drive counterclockwise with squared X (maintaining sign)
                 }  
